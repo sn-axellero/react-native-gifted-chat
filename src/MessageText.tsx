bd6ade82afac4da9e16bfeb1900262672204d71c
sn-axellero <55737874+sn-axellero@users.noreply.github.com>
2019-12-20 15:09:26 +0100

    Update MessageText.tsx

--- src/MessageText.tsx
+++ src/MessageText.tsx
@@ -87,7 +87,7 @@ export interface MessageTextProps<TMessage extends IMessage> {
   textProps?: TextProps
   customTextStyle?: StyleProp<TextStyle>
   parsePatterns?(linkStyle: TextStyle): any
-  onLinkPress?(meta: any, url: String): any
+  onLinkPress?(meta: any, url: string): any
 }
 
 export default class MessageText<
